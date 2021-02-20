c048bda0d0c0fae1bc65288ecc85bf817cbebbe9
MontseCarr <78034427+MontseCarr@users.noreply.github.com>
2021-02-19 22:46:09 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,17 @@
-/* Put your javascript in here */
+let width = 130; 
+let count = 3; 
+let list = package.querySelector("ul");
+let listElems = package.querySelectorAll("li");
+let position = 0;
+
+package.querySelector(".prev").onclick = function () {
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+};
+
+package.querySelector(".next").onclick = function () {
+  position -= width * count;
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+};
